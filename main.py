ff2ecf1ee6e2e0879df41cf40a8e528db17adf91
RomanZemlin <roman.zemlin@mail.ru>
2023-08-31 20:50:03 +0300

    файл запуска

---
+++ main.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import shutil
+import sys
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        with open("index.html", "r", encoding='utf-8') as f:
+            page_content = f.read()
+            shutil.copyfileobj(f, sys.stdout)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
